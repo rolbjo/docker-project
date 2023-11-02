ef66654a6182bd49788d5836f47cfb8e8faa347a
Roland Björndal <bjorndahl95@hotmail.com>
2023-11-02 14:01:05 +0100

    Add databaseURL to renders database

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,24 @@
 const express = require('express'),
   path = require('path')
+const dotenv = require('dotenv'),
+  { Client } = require('pg')
+
+dotenv.config()
+
+const client = new Client({
+  connectionString: process.env.PGURI,
+})
+
+client.connect()
 
 const app = express()
 
-app.get('/api', (_request, response) => {
-  response.send({ hello: 'World' })
+app.get('/api', async (_request, response) => {
+  const { rows } = await client.query('SELECT * FROM cities WHERE name = $1', [
+    'Stockholm',
+  ])
+
+  response.send(rows)
 })
 
 app.use(express.static(path.join(path.resolve(), 'public')))
