368f4bbdeb06425894e934ef3ced33d5d64c19a1
Roland Björndal <bjorndahl95@hotmail.com>
2023-11-05 10:02:21 +0100

    first version complete

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express'),
   path = require('path')
 const dotenv = require('dotenv'),
   { Client } = require('pg')
+const bodyParser = require('body-parser')
 
 dotenv.config()
 
@@ -13,12 +14,39 @@ client.connect()
 
 const app = express()
 
-app.get('/api', async (_request, response) => {
-  const { rows } = await client.query('SELECT * FROM cities WHERE name = $1', [
-    'Stockholm',
-  ])
+app.use(bodyParser.json())
 
-  response.send(rows)
+app.get('/api/:category', async (req, res) => {
+  const { category } = req.params
+  const { rows } = await client.query(`SELECT * FROM ${category}`)
+  res.json(rows)
+})
+
+app.post('/addUser', async (req, res) => {
+  const { name } = req.body
+  try {
+    await client.query('INSERT INTO users (name) VALUES ($1)', [name])
+    res.status(200).json({ message: 'User added successfully', userName: name })
+  } catch (error) {
+    res.status(500).json({ message: 'Error adding user', error: error.message })
+  }
+})
+
+app.get('/getWelcomeMessage', async (req, res) => {
+  try {
+    const { rows } = await client.query(
+      'SELECT name FROM users ORDER BY id DESC LIMIT 1'
+    )
+    if (rows.length > 0) {
+      res.json({ message: `Welcome, ${rows[0].name}!` })
+    } else {
+      res.json({ message: 'Welcome!' })
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'Error fetching welcome message', error: error.message })
+  }
 })
 
 app.use(express.static(path.join(path.resolve(), 'public')))
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,127 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
+import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [randomQuestion, setRandomQuestion] = useState(null)
+  const [questions, setQuestions] = useState({
+    addition: [],
+    subtraction: [],
+    division: [],
+    multiplication: [],
+  })
+  const [userAnswer, setUserAnswer] = useState('')
+  const [displayMessage, setDisplayMessage] = useState(false)
+  const [userName, setUserName] = useState('')
+  const [welcomeMessage, setWelcomeMessage] = useState('')
+
+  const fetchData = async (category) => {
+    const response = await fetch(`/api/${category}`)
+    const data = await response.json()
+    setQuestions((prevQuestion) => ({ ...prevQuestion, [category]: data }))
+  }
 
   useEffect(() => {
-    fetch('/api')
+    fetchData('addition')
+    fetchData('subtraction')
+    fetchData('division')
+    fetchData('multiplication')
+  }, [])
+
+  const handleNameSubmit = async () => {
+    if (userName.trim() === '') {
+      return
+    }
+
+    const response = await fetch('/addUser', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: userName }),
+    })
+
+    if (response.status === 200) {
+      const data = await response.json()
+      setWelcomeMessage(`Welcome, ${data.userName}!`)
+      setUserName('')
+    }
+  }
+
+  useEffect(() => {
+    fetch('/getWelcomeMessage')
       .then((response) => response.json())
-      .then((result) => {
-        alert(`Hello ${result.hello}!`)
+      .then((data) => {
+        if (data.message) {
+          setWelcomeMessage(data.message)
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching welcome message:', error)
       })
   }, [])
 
+  const getRandomQuestion = (category) => {
+    if (questions[category].length > 0) {
+      const randomNumber = Math.floor(
+        Math.random() * questions[category].length
+      )
+      setRandomQuestion(questions[category][randomNumber])
+      setDisplayMessage(false)
+    }
+  }
+
+  const categories = ['addition', 'subtraction', 'division', 'multiplication']
+
+  const createButtons = () => {
+    return categories.map((category) => (
+      <button
+        style={{ marginLeft: 20 }}
+        key={category}
+        onClick={() => getRandomQuestion(category)}
+      >
+        Give me {category}
+      </button>
+    ))
+  }
+
+  const handleInputChange = (e) => {
+    setUserAnswer(e.target.value)
+    setDisplayMessage(false)
+  }
+
+  const checkAnswer = () => {
+    setDisplayMessage(true)
+  }
+
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='mainContainer'>
+      <h1>Math Quiz</h1>
+      <input
+        type='text'
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button onClick={handleNameSubmit}>Add User</button>
+      <p>{welcomeMessage}</p>
+      <div>{createButtons()}</div>
+      {randomQuestion && (
+        <div className='questionContainer'>
+          <h2>Random Question:</h2>
+          <p>{randomQuestion.question} =</p>
+          <input type='text' value={userAnswer} onChange={handleInputChange} />
+          <button onClick={checkAnswer}>Check Your Answer</button>
+          {displayMessage && (
+            <p>
+              {userAnswer.trim() === ''
+                ? 'Please enter an answer'
+                : parseInt(userAnswer) === randomQuestion.answer
+                ? 'That is correct!'
+                : 'Try again'}
+            </p>
+          )}
+        </div>
+      )}
+    </div>
   )
 }
 
